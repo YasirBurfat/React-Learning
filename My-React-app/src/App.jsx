6a2413b9d970857e0f3a601076ae5674b6082e99
Yasir Burfat <yasirburfat022@gmail.com>
2025-06-28 22:34:47 +0500

    React Learning code and A Project

---
+++ My-React-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ProductTab from "./ProductTab";
+
+function Title(){
+  return <h1>I am Title</h1>;
+}
+
+function App() {
+  return (
+    <ProductTab />
+  );
+}
+
+export default App
---
+++ My-React-app/src/Product.jsx
@@ -0,0 +1,10 @@
+function Product(){
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>Description</p>
+        </div>
+    );
+}
+
+export default Product;
